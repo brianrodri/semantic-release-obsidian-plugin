e63b12811e1c81918c366a9be05da28d9ea84e33
Brian Rodriguez <brian@brianrodri.com>
2024-04-09 18:30:57 -0400

    chore: setup eslint and prettier

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import js from "@eslint/js";
+import prettierConfig from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
+
+/** @type { import("eslint").Linter.FlatConfig[] } */
+export default [
+    { ignores: ["coverage/", "node_modules/"] },
+
+    js.configs.recommended,
+
+    {
+        plugins: { prettier: prettierPlugin },
+        rules: {
+            ...prettierConfig.rules,
+            "prettier/prettier": "error",
+        },
+    },
+];
