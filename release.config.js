461e160ae12f3e4206dc84acbe4982027fae7725
Brian Rodriguez <brian@brianrodri.com>
2024-04-10 07:45:33 -0400

    build: use default release steps

--- release.config.js
+++ release.config.js
@@ -1,13 +1,2 @@
 /** @type { import("@semantic-release/release-config").ReleaseConfig } */
 export const branches = ["main"];
-export const plugins = [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "brianrodri/semantic-release-obsidian-plugin",
-    [
-        "@semantic-release/github",
-        {
-            assets: ["package.json", "package-lock.json", "manifest.json", "versions.json"],
-        },
-    ],
-];
