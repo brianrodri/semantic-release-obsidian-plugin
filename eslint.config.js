b9ffefb4c4243ae0766aed836c240a47b8d4163d
Brian Rodriguez <brian@brianrodri.com>
2025-01-01 15:58:27 -0500

    chore: fix deprecated types

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import js from "@eslint/js";
 import prettierConfig from "eslint-config-prettier";
 import prettierPlugin from "eslint-plugin-prettier";
 
-/** @type { import("eslint").Linter.FlatConfig[] } */
+/** @type { import("eslint").Linter.Config[] } */
 export default [
     { ignores: ["coverage/", "node_modules/"] },
 
