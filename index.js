446c52d91581b2be33e5e4f116cd9fc9e2f31ca6
Brian Rodriguez <brian@brianrodri.com>
2025-05-11 16:40:11 -0400

    feat: remove redundant verification logic

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 import { prepare as prepareImpl } from "./src/prepare.js";
 import { verifyConditions as verifyConditionsImpl } from "./src/verify.js";
 
-let verified = false;
-
 export async function verifyConditions(pluginConfig, context) {
     await verifyConditionsImpl(pluginConfig, context);
-    verified = true;
 }
 
 export async function prepare(pluginConfig, context) {
-    if (!verified) {
-        await verifyConditions(pluginConfig, context);
-    }
     await prepareImpl(pluginConfig, context);
 }
